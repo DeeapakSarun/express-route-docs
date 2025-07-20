e5922cc511f93dcfd99defb8e09ed6fd27113a94
deeapak Sarun <dyuvachandran@slu.edu>
2025-07-20 16:09:26 -0500

    fixed route issues

--- index.js
+++ index.js
@@ -4,17 +4,8 @@ const path = require("path");
 const { setupRouteTracking } = require("./routesTracker.js");
 const { docsHandler } = require("./routesDocs");
 
-function expressRouteDocs(app, options = {}) {
-  // Enable tracking
-  setupRouteTracking(app);
-
-  // Serve static assets
-  app.use("/docs/static", express.static(path.join(__dirname, "public")));
-
-  // Register the docs route
-  app.get("/docs", docsHandler);
-}
 
 module.exports = {
-  expressRouteDocs,
+  setupRouteTracking,
+  docsHandler
 };
